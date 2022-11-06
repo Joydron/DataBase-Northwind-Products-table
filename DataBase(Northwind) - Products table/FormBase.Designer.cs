210eab2faeec34accac09c7f57462427073edf84
Joydron <ishimianderson@gmail.com>
2022-11-06 16:55:43 +0500

    first commit

---
+++ DataBase(Northwind) - Products table/FormBase.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DataBase_Northwind____Products_table
+{
+    partial class ProductsTable
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_Add = new System.Windows.Forms.DataGridView();
+            this.dataGridView_Base = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.nameSearch = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pctSearch = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Add)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Base)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_Add
+            // 
+            this.dataGridView_Add.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Add.Location = new System.Drawing.Point(1, 263);
+            this.dataGridView_Add.Name = "dataGridView_Add";
+            this.dataGridView_Add.Size = new System.Drawing.Size(787, 186);
+            this.dataGridView_Add.TabIndex = 0;
+            // 
+            // dataGridView_Base
+            // 
+            this.dataGridView_Base.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Base.Location = new System.Drawing.Point(1, 40);
+            this.dataGridView_Base.Name = "dataGridView_Base";
+            this.dataGridView_Base.Size = new System.Drawing.Size(787, 186);
+            this.dataGridView_Base.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(652, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(136, 20);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // nameSearch
+            // 
+            this.nameSearch.AutoSize = true;
+            this.nameSearch.Location = new System.Drawing.Point(552, 15);
+            this.nameSearch.Name = "nameSearch";
+            this.nameSearch.Size = new System.Drawing.Size(94, 13);
+            this.nameSearch.TabIndex = 3;
+            this.nameSearch.Text = "Поиск продуктов";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Image = global::DataBase_Northwind____Products_table.Properties.Resources.Save_icon;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(103, 234);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Сохранить данные";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::DataBase_Northwind____Products_table.Properties.Resources.add_icon;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(13, 232);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(84, 25);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pctSearch
+            // 
+            this.pctSearch.Image = global::DataBase_Northwind____Products_table.Properties.Resources.search_icon;
+            this.pctSearch.Location = new System.Drawing.Point(522, 12);
+            this.pctSearch.Name = "pctSearch";
+            this.pctSearch.Size = new System.Drawing.Size(24, 22);
+            this.pctSearch.TabIndex = 6;
+            this.pctSearch.TabStop = false;
+            // 
+            // ProductsTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pctSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.nameSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dataGridView_Base);
+            this.Controls.Add(this.dataGridView_Add);
+            this.Name = "ProductsTable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Add)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Base)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctSearch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_Add;
+        private System.Windows.Forms.DataGridView dataGridView_Base;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label nameSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pctSearch;
+    }
+}
+
---
+++ DataBase(Northwind) - Products table/FormBase.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase_Northwind____Products_table
+{
+    public partial class ProductsTable : Form
+    {
+        private SqlConnection nrthwndConnection = null;
+
+        public ProductsTable()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Подключаем БД
+            nrthwndConnection = new SqlConnection
+                (ConfigurationManager.ConnectionStrings["NorthWindDB"].ConnectionString);
+            nrthwndConnection.Open();
+
+            // Выбираем все данные из Products
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Products", nrthwndConnection);
+            DataSet db = new DataSet();
+            dataAdapter.Fill(db);
+            dataGridView_Base.DataSource = db.Tables[0];
+
+            // Создаем checkBox для dataGridView_Base
+            DataGridViewCheckBoxColumn chkbox = new DataGridViewCheckBoxColumn() 
+                { HeaderText = "Выбрать", 
+                  Name = "checkBox" };
+            dataGridView_Base.Columns.Insert(0, chkbox);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // Создаем быстрый поиск продуктов
+            (dataGridView_Base.DataSource as DataTable).DefaultView.RowFilter = $"ProductName LIKE '%{txtSearch.Text}%'";
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Сохранение данных
+
+            TextWriter data = new StreamWriter(@"D:\\text file.txt");
+            int rowcount = dataGridView_Add.Rows.Count-1;
+
+            // Цикл для сохранения строки
+            for (int i = 0; i < rowcount; i++)
+            {
+                // Цикл для сохранения ячеек (без столбца true)
+                for (int j = 1 ; j < 9; j++)
+                {
+                    data.Write(dataGridView_Add.Rows[i].Cells[j].Value.ToString() + "\n");
+                }
+                data.Write("\n");
+
+            }
+            data.Close();
+            MessageBox.Show("Сохранено на диск D");
+
+        }
+
+        // Добавляем выбранные файлы из таблицы и добавляем наименования колонн из 1-ой таблицы 
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            DataTable dataTable = new DataTable();
+
+            // Добавляем цикл для названий колонн
+            foreach (DataGridViewColumn column in dataGridView_Base.Columns)
+            {
+                dataTable.Columns.Add(column.Name);
+            }
+
+            // Проверка на checkBox
+            foreach (DataGridViewRow drvRow in dataGridView_Base.Rows)
+            {
+                bool chkboxselect = Convert.ToBoolean(drvRow.Cells["checkBox"].Value);
+                if (chkboxselect)
+                {
+                    DataRow dataRow = dataTable.NewRow();  
+                    
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        dataRow[i] = drvRow.Cells[i].Value;
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+                dataGridView_Add.DataSource = dataTable;
+            }
+        }
+    }
+
+}
